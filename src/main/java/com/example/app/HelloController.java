8f4a72e8608be5f1cedd37d04c7543f21e61311e
SyedAliHassan512 <shamsialihassan512@gmail.com>
2025-12-05 00:45:57 +0500

    first commit

---
+++ src/main/java/com/example/app/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Welcome to Syed Ali Hassan Java Cicd";
+    }
+}
